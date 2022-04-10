abd8bcdc815f9a188488476e2f6e4736ee1b4cc4
kamkow1 <ikamileq@gmail.com>
2022-04-10 15:36:16 -0700

    added file system support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
 import { lexer } from './lexer/lexer'
+import { Command } from 'commander'
+import fs from 'fs'
+
+const app = new Command();
+
+app.name('wrn');
+
+app
+    .command('exec')
+    .argument('<string>', 'path to .wr file')
+    .action((str) => {
+        let path = str;
+
+        const pathElements = str.split('/');
+        const name = pathElements[pathElements.length - 1];
+
+        if (!name.includes('.wr')) {
+
+            const pathWithIndex = path + '/index.wr';
+            console.log(pathWithIndex);
+
+            if (fs.existsSync(pathWithIndex)) {
+                path = pathWithIndex;
+            } else {
+                console.error('file extension not recognized! only .ks files are executable');
+                return;
+            }
+        }
+
+        const code = fs.readFileSync(path).toString('ascii');
+        const lexedCode = lexer.reset(code);
+    })
+
+
+app.parse();
+
 
 console.log('start');
--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -3,7 +3,7 @@ import { config } from './config';
 
 export const lexer = moo.compile(config);
 
-lexer.reset('abc = 6');
+//lexer.reset('abc = 6');
 
 while(true) {
     const token = lexer.next();
