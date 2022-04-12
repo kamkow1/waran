e97890c6094310c061d97ba6a5d4c787d656eddc
kamkow1 <ikamileq@gmail.com>
2022-04-12 08:57:34 -0700

    fixed grammar

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -1,8 +1,8 @@
 import moo from 'moo'
 
 export const lexer = moo.compile({
+    NL:             { match: /\r\n/, lineBreaks: true },
     WS:             /[ \t]+/,
-    NL:             { match: /\n/, lineBreaks: true },
     comment:        /\/\/.*?$/,
     number:         /0|[1-9][0-9]*/,
     string:         /"(?:\\["\\]|[^\n"\\])*"/,
