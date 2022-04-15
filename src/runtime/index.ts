374a262cb9a18ed87d78910912d599840aeb76e4
kamkow1 <ikamileq@gmail.com>
2022-04-14 17:12:32 -0700

    code cleanup #2

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -7,17 +7,3 @@ function add(...args) {
         return prev + curr;
     });
 }
-
-export const runtimeString = `
-
-function std_out(...args) {
-    console.log(...args);
-}
-
-function add(...args) {
-    return args.reduce((prev, curr) => {
-        return prev + curr;
-    });
-}
-
-`;
