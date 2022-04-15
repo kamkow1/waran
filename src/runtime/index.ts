acacf1a26f6e019194c4c9bb36fe933f627e49d9
kamkow1 <ikamileq@gmail.com>
2022-04-15 05:51:18 -0700

    runtime: math functions

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,9 +1,34 @@
 function std_out(...args) {
-    console.log(...args);
+    let text = args.join(' ');
+    process.stdout.write(text);
 }
 
 function add(...args) {
     return args.reduce((prev, curr) => {
         return prev + curr;
+    });
+}
+
+function mult(...args) {
+    return args.reduce((prev, curr) => {
+        return prev * curr;
+    });
+}
+
+function div(...args) {
+    return args.reduce((prev, curr) => {
+        return prev / curr;
+    });
+}
+
+function subtr(...args) {
+    return args.reduce((prev, curr) => {
+        return prev - curr;
+    });
+}
+
+function pow(...args) {
+    return args.reduce((prev, curr) => {
+        return prev ** curr;
     });
 }
