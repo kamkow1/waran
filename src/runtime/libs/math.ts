7173f3c96adf4a1d6b639e6bb8940dfcc0d9c39c
kamkow1 <ikamileq@gmail.com>
2022-04-16 23:41:25 +0200

    libs: strong typing for math lib

--- src/runtime/libs/math.ts
+++ src/runtime/libs/math.ts
@@ -1,28 +1,28 @@
-function add(...args) {
+function add(...args: number[]) {
     return args.reduce((prev, curr) => {
         return prev + curr;
     });
 }
 
-function mult(...args) {
+function mult(...args: number[]) {
     return args.reduce((prev, curr) => {
         return prev * curr;
     });
 }
 
-function div(...args) {
+function div(...args: number[]) {
     return args.reduce((prev, curr) => {
         return prev / curr;
     });
 }
 
-function subtr(...args) {
+function subtr(...args: number[]) {
     return args.reduce((prev, curr) => {
         return prev - curr;
     });
 }
 
-function pow(...args) {
+function pow(...args: number[]) {
     return args.reduce((prev, curr) => {
         return prev ** curr;
     });
